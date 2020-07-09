9e9c281cb96bded02e0f003b45cba855d9633feb
Stefan Maierhofer <sm@stefanmaierhofer.com>
2020-07-09 09:33:52 +0200

    update build system

--- deploy.cmd
+++ deploy.cmd
@@ -8,4 +8,6 @@ git tag %1
 git push --tags
 
 dotnet paket pack bin --version %1
-dotnet nuget push "bin\*.%1.nupkg" --skip-duplicate --source https://api.nuget.org/v3/index.json
+
+set /p Key=<%HOMEPATH%\.ssh\nuget.key
+dotnet nuget push "bin/**.%1.nupkg" --skip-duplicate --source https://api.nuget.org/v3/index.json -k %Key%
